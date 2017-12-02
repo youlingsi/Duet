179aa83fe3ea65b209e4d29d26b8ff705a24f7e4
Grant Wu <grantwwu@gmail.com>
2017-12-02 17:55:07 -0500

    Add Emoji_Test

---
+++ Assets/_Script/Emoji_Test.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class Emoji_Test : MonoBehaviour
+{
+
+    private const string url = "http://grantwu.me:5000/galaga_emoji";
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetText());
+	}
+	
+	IEnumerator GetText()
+    {
+        while(true)
+        {
+            WWW www = new WWW(url);
+            yield return www;
+
+            setNextType(www.text);
+
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+    void setNextType(string text)
+    {
+        var N = JSON.Parse(text);
+        int pogChamp = N["PogChamp"];
+        int triHard = N["TriHard"];
+        int kappa = N["Kappa"];
+
+        if (kappa > triHard && kappa > pogChamp)
+        {
+            type = 1;
+            print("Current wave: Kappa");
+        }
+        if (triHard > kappa && triHard > pogChamp)
+        {
+            type = 2;
+            print("Current wave: TriHard");
+        }
+        if (pogChamp > kappa && pogChamp > triHard)
+        {
+            type = 3;
+            print("Current wave: PogChamp");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
