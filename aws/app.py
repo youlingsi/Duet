64e69f0db082ca0982f192a88d8a13407fc5fa4a
Grant Wu <grantwwu@gmail.com>
2017-12-06 10:47:10 -0500

    Add expiry to the AWS part

--- aws/app.py
+++ aws/app.py
@@ -1,21 +1,28 @@
 import json
+import time
 from flask import Flask, request
 
 app = Flask(__name__)
 
 emoji_dict = {}
+last_time = time.time()
 
 
 @app.route("/duet_emoji", methods=["GET"])
 def read():
-    return json.dumps(emoji_dict)
+    global last_time
+    if time.time() - last_time > 10:
+        return json.dumps({"emoji": "", "count": 0})
+    else:
+        return json.dumps(emoji_dict)
 
 
 @app.route("/duet_emoji", methods=["POST"])
 def write():
-    global emoji_dict
+    global emoji_dict, last_time
     param = request.get_json(force=True, silent=True)
     emoji_dict = param if param else emoji_dict
+    last_time = time.time()
     return ("", 204)
 
 
