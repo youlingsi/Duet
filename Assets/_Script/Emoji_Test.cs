84d4354579af9d94a414ce0586930b8b0e1609ee
Grant Wu <grantwwu@gmail.com>
2017-12-05 21:49:13 -0500

    Changes in scenes/script needed

--- Assets/_Script/Emoji_Test.cs
+++ Assets/_Script/Emoji_Test.cs
@@ -9,7 +9,8 @@ public class Emoji_Test : MonoBehaviour
 
     private const string url = "http://grantwu.me:5000/duet_emoji";
     private bool seen = false;
-    private AudioSource com;
+    public AudioSource com;
+    public AudioClip[] clips;
     private string emoji = "";
     private int count = 0;
 	// Use this for initialization
@@ -43,24 +44,28 @@ public class Emoji_Test : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        // if (!seen)
-        // {
-        //     seen = true;
-        //     com.Stop();
-        //     com.volume = 1f; // Do something with count here
-        //     if (emoji == "PogChamp")
-        //     {
-        //         com.clip = null; // Fill this in
-        //         com.Play();
-        //     }
-        //     else if (emoji == "Kappa")
-        //     {
-        //         com.clip = null; // Fill this in
-        //         com.Play();
-        //     } // etc.
-        //     else
-        //     {
-        //     }
-        // }
+        if (!seen)
+        {
+            seen = true;
+            com.Stop();
+            com.volume = 1f; // Do something with count here
+            // if (emoji == "PogChamp")
+            // For testing purposes I swapped in TriHard
+            if (emoji == "TriHard")
+            {
+                com.clip = clips[0]; // Fill this in
+                com.Play();
+            }
+            else if (emoji == "Kappa")
+            {
+                com.clip = clips[1];
+                com.Play();
+            }
+            else if (emoji == "BibleThump")
+            {
+                com.clip = clips[2];
+                com.Play();
+            }
+        }
 	}
 }
